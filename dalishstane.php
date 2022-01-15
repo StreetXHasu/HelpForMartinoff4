70e09f41474860a7ec420a50d380f213ff62b925
M.S.4 <martinoff.2022@gmail.com>
2022-01-15 20:23:35 +0200

    test2

--- dalishstane.php
+++
@@ -1,8 +0,0 @@
-<?php
-$redirect_link=$_REQUEST['page_url'];
-                    if($redirect_link==""){
-                        header("location: index.php");
-                    } else{
-                        header("location: ".$redirect_link);
-                    }
-                    ?>
