0a884eaf43b93602776e029a6bd8b28fc2ecb70c
StreetXHasu <nvmihasu@gmail.com>
2022-01-16 15:57:46 +0300

    fix sort and remove sql_mode

--- php/config.php
+++ php/config.php
@@ -11,6 +11,4 @@ if(!$conn) {
     die("Connection failed: ".mysqli_connect_error());
 }
 
-//TODO https://stackoverflow.com/questions/34115174/error-related-to-only-full-group-by-when-executing-a-query-in-mysql
-mysqli_query($conn, "set session sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
 ?>
