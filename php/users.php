09e4ed587dafbb24265838aaeec11b712765daf8
M.S.4 <martinoff.2022@gmail.com>
2022-01-16 13:10:14 +0200

    last changes

--- php/users.php
+++ php/users.php
@@ -2,7 +2,7 @@
     session_start();
     include_once "config.php";
     $outgoing_id = $_SESSION['unique_id'];
-    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
+    $sql = "SELECT * FROM users LEFT JOIN messages ON users.unique_id = messages.incoming_msg_id  WHERE NOT unique_id = {$outgoing_id} ORDER BY created_at DESC";
     
     $query = mysqli_query($conn, $sql);
     $output = "";
