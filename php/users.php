d8808468efde0ffbaeceb3f975c4c77beb7a1b95
StreetXHasu <nvmihasu@gmail.com>
2022-01-16 16:42:54 +0300

    like this

--- php/users.php
+++ php/users.php
@@ -1,16 +1,16 @@
 <?php
-    session_start();
-    include_once "config.php";
-    $outgoing_id = $_SESSION['unique_id'];
-    //need group by
+session_start();
+include_once "config.php";
+$outgoing_id = $_SESSION['unique_id'];
+//need group by
 $sql = "SELECT
-	*
+	users.*
 FROM
 	users
-	 LEFT JOIN messages ON users.unique_id = messages.incoming_msg_id 
-WHERE NOT unique_id = '$outgoing_id'
+	JOIN messages ON users.unique_id = messages.incoming_msg_id OR users.unique_id = messages.outgoing_msg_id
+WHERE  users.unique_id !='$outgoing_id' and (messages.incoming_msg_id='$outgoing_id' OR  messages.outgoing_msg_id='$outgoing_id')
 GROUP BY
-	user_id 
+	user_id
 ORDER BY
 	MAX(messages.created_at) DESC";
 
@@ -19,18 +19,17 @@ try {
   $query = mysqli_query($conn, $sql);
   $output = "";
   echo mysqli_error($conn);
-  if(mysqli_num_rows($query) == 0){
+  echo $sql;
+  if (mysqli_num_rows($query) == 0) {
     $output .= "Not enough registered users";
-  }elseif(mysqli_num_rows($query) > 0){
+  } elseif (mysqli_num_rows($query) > 0) {
     include_once "data.php";
 
   }
   echo $output;
-}catch (Exception $err){
+} catch (Exception $err) {
   echo $err;
 }
-
-//    $sql = "SELECT * FROM users LEFT JOIN messages ON users.unique_id = messages.incoming_msg_id   ORDER BY created_at DESC";
 
 
 ?>
