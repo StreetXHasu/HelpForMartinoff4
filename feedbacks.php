040b771d59da7ea4cec0e04769984579658bed6e
StreetXHasu <69196228+StreetXHasu@users.noreply.github.com>
2022-01-19 13:42:40 +0300

    Check sign

--- feedbacks.php
+++ feedbacks.php
@@ -1,25 +1,25 @@
-<?php 
-  session_start();
-  include_once "php/config.php";
-  include_once "php/protocol.php";
-  if(!isset($_SESSION['unique_id'])){
-    header("location: login.php?page_url=". $redirect_link_var );
-  }
+<?php
+session_start();
+include_once "php/config.php";
+include_once "php/protocol.php";
+if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php?page_url=" . $redirect_link_var);
+}
 ?>
-<?php include_once "header.php"; 
+<?php include_once "header.php";
 ?>
 
 <body>
-    <div>Feedbacks</div>
+<div>Feedbacks</div>
 
 
-    <script>
+<script>
     var active = document.querySelector(".navbtn-4");
     active.classList.add("active");
 </script>
 <?php
-if(isset($_SESSION['unique_id'])){
-  echo '<script src="javascript/profile-nav.js"></script>';
+if (isset($_SESSION['unique_id'])) {
+    echo '<script src="javascript/profile-nav.js"></script>';
 }
-  ?>
+?>
 </body>
