9f1cbeedd056252c09f70fb64ec5014b4832222b
StreetXHasu <69196228+StreetXHasu@users.noreply.github.com>
2022-01-19 13:57:05 +0300

    signature verification

--- login.php
+++ login.php
@@ -1,39 +1,38 @@
-<?php 
-  session_start();
-  include_once "php/config.php";
-  if(isset($_SESSION['unique_id'])){
+<?php
+session_start();
+include_once "php/config.php";
+if (isset($_SESSION['unique_id'])) {
     header("location: index.php");
-  }
+}
 ?>
 
 <?php include_once "header.php"; ?>
 <body>
 <section class="centered-form">
-  <div class="wrapper">
-    <section class="form login">
-      <header>Вход</header>
-      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
-        <div class="error-text"></div>
-        <div class="field input">
-          <label>Въведи имейл</label>
-          <input type="text" name="email" placeholder="Въведи своя имейл" required>
-        </div>
-        <div class="field input">
-          <label>Парола</label>
-          <input type="password" name="password" placeholder="Въведи своята парола" required>
-          <i class="fas fa-eye"></i>
-        </div>
-        <div class="field button">
-          <input type="submit" name="submit" value="Влез в акаунта">
-        </div>
-      </form>
-      <div class="link">Нямаш акаунт? <a href="signup.php">Регистрирай се!</a></div>
-    </section>
-  </div>
-  </section>
-  <script src="javascript/pass-show-hide.js"></script>
-  <script src="javascript/login3.js"></script>
-
+    <div class="wrapper">
+        <section class="form login">
+            <header>Вход</header>
+            <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+                <div class="error-text"></div>
+                <div class="field input">
+                    <label>Въведи имейл</label>
+                    <input type="text" name="email" placeholder="Въведи своя имейл" required>
+                </div>
+                <div class="field input">
+                    <label>Парола</label>
+                    <input type="password" name="password" placeholder="Въведи своята парола" required>
+                    <i class="fas fa-eye"></i>
+                </div>
+                <div class="field button">
+                    <input type="submit" name="submit" value="Влез в акаунта">
+                </div>
+            </form>
+            <div class="link">Нямаш акаунт? <a href="signup.php">Регистрирай се!</a></div>
+        </section>
+    </div>
+</section>
+<script src="javascript/pass-show-hide.js"></script>
+<script src="javascript/login3.js"></script>
 
 
 </body>
